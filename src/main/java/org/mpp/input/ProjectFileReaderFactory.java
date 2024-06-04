a7fdb2265e620ba25aa26d8a62b55a78ab5ef5a7
sangeerththan <sangeerththan.15@cse.mrt.ac.lk>
2024-06-04 15:41:53 +0530

    [REFACTOR] - Reader changed to universal projectreader

--- src/main/java/org/mpp/input/ProjectFileReaderFactory.java
+++ src/main/java/org/mpp/input/ProjectFileReaderFactory.java
@@ -1,13 +1,15 @@
 package org.mpp.input;
 
 import net.sf.mpxj.ProjectFile;
-import net.sf.mpxj.mpp.MPPReader;
+
+import net.sf.mpxj.reader.UniversalProjectReader;
+
 import java.io.File;
 
 public class ProjectFileReaderFactory {
 
     public static ProjectFile readProjectFile(String inputFilePath) throws Exception {
         File inputFile = new File(inputFilePath);
-        return new MPPReader().read(inputFile);
+        return new UniversalProjectReader().read(inputFile);
     }
 }
