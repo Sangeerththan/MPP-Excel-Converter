b3519fff8b1ad0c92c8955b8628ff93f1efddc21
Sangeerththan Balachandran <sangeerththan.15@cse.mrt.ac.lk>
2024-06-28 19:42:45 +0530

    Update convert-html-to-image.js

--- convert-html-to-image.js
+++ convert-html-to-image.js
@@ -10,14 +10,16 @@ const path = require('path');
     const filePath = path.resolve(process.env.RUNNER_TEMP, 'reports/dependency-check-report.html');
     const fileUrl = 'file:///' + filePath.replace(/\\/g, '/'); // Convert backslashes to forward slashes for Windows compatibility
 
+    console.log('Navigating to:', fileUrl);
     await page.goto(fileUrl, { waitUntil: 'networkidle2' });
 
     // Save the screenshot to the temporary directory
     const outputFilePath = path.resolve(process.env.RUNNER_TEMP, 'reports/dependency-check-report.png');
     await page.screenshot({ path: outputFilePath });
 
-    await browser.close();
     console.log('Screenshot saved successfully:', outputFilePath);
+
+    await browser.close();
   } catch (error) {
     console.error('Error capturing screenshot:', error);
     process.exit(1); // Exit with non-zero code on error to fail the GitHub Action
