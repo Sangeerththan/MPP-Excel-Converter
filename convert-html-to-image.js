5cf8bc3c864b9ef0e62835b100c8da8116c25f97
Sangeerththan Balachandran <sangeerththan.15@cse.mrt.ac.lk>
2024-06-28 19:12:58 +0530

    Update convert-html-to-image.js

--- convert-html-to-image.js
+++ convert-html-to-image.js
@@ -4,15 +4,16 @@ const path = require('path');
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  
+
   // Construct the file URL
   const filePath = path.resolve(process.env.GITHUB_WORKSPACE, 'reports/dependency-check-report.html');
   const fileUrl = 'file:///' + filePath;
-  
-  await page.goto(fileUrl, { waitUntil: 'networkidle2' });
-  const fileImagePath = path.resolve(process.env.GITHUB_WORKSPACE, 'reports/dependency-check-report.png');
 
-  await page.screenshot({ path: fileImagePath });
+  await page.goto(fileUrl, { waitUntil: 'networkidle2' });
+
+  // Save the screenshot to the temporary directory
+  const outputFilePath = path.resolve(process.env.RUNNER_TEMP, 'dependency-check-report.png');
+  await page.screenshot({ path: outputFilePath });
 
   await browser.close();
 })();
